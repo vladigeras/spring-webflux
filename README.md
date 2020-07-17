ac430d0238fc80bce5717b96ef4328f1e1f55b68
Vladislav Gerasimov <gerasimov.v88@gmail.com>
2020-07-17 15:10:54 +0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# Description
+This project demonstrates Reactive approach for web applications 
+with [Spring Boot WebFlux](https://docs.spring.io/spring/docs/current/spring-framework-reference/web-reactive.html) module.
---
+++ src/main/java/ru/vladigeras/springwebflux/SpringWebfluxApplication.java
@@ -0,0 +1,13 @@
+package ru.vladigeras.springwebflux;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringWebfluxApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringWebfluxApplication.class, args);
+	}
+
+}
